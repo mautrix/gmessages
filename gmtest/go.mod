f2853d78b593c1a1e6c4b3d880ecc81bbe8a8408
Tulir Asokan <tulir@maunium.net>
2023-06-30 16:27:50 +0300

    Add cli test tool for libgm

---
+++ gmtest/go.mod
@@ -0,0 +1,21 @@
+module go.mau.fi/mautrix-gmessages/gmtest
+
+go 1.20
+
+require (
+	github.com/mdp/qrterminal/v3 v3.1.1
+	github.com/rs/zerolog v1.29.1
+	go.mau.fi/mautrix-gmessages/libgm v0.1.0
+)
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
+
+replace go.mau.fi/mautrix-gmessages/libgm => ../libgm
---
+++ gmtest/main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"bufio"
+	"encoding/gob"
+	"errors"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/mdp/qrterminal/v3"
+	"github.com/rs/zerolog"
+
+	"go.mau.fi/mautrix-gmessages/libgm"
+	"go.mau.fi/mautrix-gmessages/libgm/binary"
+	"go.mau.fi/mautrix-gmessages/libgm/crypto"
+	"go.mau.fi/mautrix-gmessages/libgm/events"
+)
+
+type Session struct {
+	*libgm.DevicePair
+	*crypto.Cryptor
+	*binary.WebAuthKey
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustReturn[T any](val T, err error) T {
+	must(err)
+	return val
+}
+
+var cli *libgm.Client
+var log zerolog.Logger
+var sess Session
+
+func main() {
+	log = zerolog.New(zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
+		w.Out = os.Stdout
+		w.TimeFormat = time.Stamp
+	})).With().Timestamp().Logger()
+	file, err := os.Open("session.gob")
+	if err != nil {
+		if !errors.Is(err, os.ErrNotExist) {
+			panic(err)
+		}
+	} else {
+		must(gob.NewDecoder(file).Decode(&sess))
+		log.Info().Msg("Loaded session?")
+	}
+	_ = file.Close()
+	if sess.Cryptor == nil {
+		sess.Cryptor = crypto.NewCryptor(nil, nil)
+	}
+	cli = libgm.NewClient(sess.DevicePair, sess.Cryptor, log, nil)
+	cli.SetEventHandler(evtHandler)
+	if sess.DevicePair == nil {
+		pairer := mustReturn(cli.NewPairer(nil, 20))
+		registered := mustReturn(pairer.RegisterPhoneRelay())
+		must(cli.Connect(registered.Field5.RpcKey))
+	} else {
+		must(cli.Connect(sess.WebAuthKey.WebAuthKey))
+	}
+
+	c := make(chan os.Signal)
+	input := make(chan string)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		defer close(input)
+		scan := bufio.NewScanner(os.Stdin)
+		for scan.Scan() {
+			line := strings.TrimSpace(scan.Text())
+			if len(line) > 0 {
+				input <- line
+			}
+		}
+	}()
+	defer saveSession()
+	for {
+		select {
+		case <-c:
+			log.Info().Msg("Interrupt received, exiting")
+			return
+		case cmd := <-input:
+			if len(cmd) == 0 {
+				log.Info().Msg("Stdin closed, exiting")
+				return
+			}
+			args := strings.Fields(cmd)
+			cmd = args[0]
+			args = args[1:]
+			//go handleCmd(strings.ToLower(cmd), args)
+		}
+	}
+}
+
+func saveSession() {
+	file := mustReturn(os.OpenFile("session.gob", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600))
+	must(gob.NewEncoder(file).Encode(sess))
+	_ = file.Close()
+}
+
+func evtHandler(rawEvt any) {
+	switch evt := rawEvt.(type) {
+	case *events.ClientReady:
+		log.Debug().Any("simData", evt.Session.Settings).Msg("Client is ready!")
+	case *events.PairSuccessful:
+		log.Debug().Any("data", evt).Msg("Pair successful")
+		sess.DevicePair = &libgm.DevicePair{
+			Mobile:  evt.PairDeviceData.Mobile,
+			Browser: evt.PairDeviceData.Browser,
+		}
+		sess.WebAuthKey = evt.PairDeviceData.WebAuthKeyData
+		saveSession()
+		log.Debug().Msg("Wrote session")
+	case *binary.Event_MessageEvent:
+		log.Debug().Any("data", evt).Msg("Message event")
+	case *binary.Event_ConversationEvent:
+		log.Debug().Any("data", evt).Msg("Conversation event")
+	case *events.QR:
+		qrterminal.GenerateHalfBlock(evt.URL, qrterminal.L, os.Stdout)
+	case *events.BrowserActive:
+		log.Debug().Any("data", evt).Msg("Browser active")
+	case *events.Battery:
+		log.Debug().Any("data", evt).Msg("Battery")
+	case *events.DataConnection:
+		log.Debug().Any("data", evt).Msg("Data connection")
+	default:
+		log.Debug().Any("data", evt).Msg("Unknown event")
+	}
+}
