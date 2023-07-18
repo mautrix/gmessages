3e2348447ac9985abda59b0bc07de16f419552fc
Tulir Asokan <tulir@maunium.net>
2023-07-19 01:20:32 +0300

    Merge response waiting methods with sending

--- libgm/session_handler.go
+++ libgm/session_handler.go
@@ -1,6 +1,7 @@
 package libgm
 
 import (
+	"encoding/base64"
 	"fmt"
 	"sync"
 	"time"
@@ -70,6 +71,49 @@ func typedResponse[T proto.Message](resp *IncomingRPCMessage, err error) (casted
 		retErr = fmt.Errorf("unexpected response type %T, expected %T", resp.DecryptedMessage, casted)
 	}
 	return
+}
+
+func (s *SessionHandler) waitResponse(requestID string) chan *IncomingRPCMessage {
+	ch := make(chan *IncomingRPCMessage, 1)
+	s.responseWaitersLock.Lock()
+	s.responseWaiters[requestID] = ch
+	s.responseWaitersLock.Unlock()
+	return ch
+}
+
+func (s *SessionHandler) cancelResponse(requestID string, ch chan *IncomingRPCMessage) {
+	s.responseWaitersLock.Lock()
+	close(ch)
+	delete(s.responseWaiters, requestID)
+	s.responseWaitersLock.Unlock()
+}
+
+func (s *SessionHandler) receiveResponse(msg *IncomingRPCMessage) bool {
+	if msg.Message == nil {
+		return false
+	}
+	requestID := msg.Message.SessionID
+	s.responseWaitersLock.Lock()
+	ch, ok := s.responseWaiters[requestID]
+	if !ok {
+		s.responseWaitersLock.Unlock()
+		return false
+	}
+	delete(s.responseWaiters, requestID)
+	s.responseWaitersLock.Unlock()
+	evt := s.client.Logger.Trace().
+		Str("request_id", requestID)
+	if evt.Enabled() {
+		if msg.DecryptedData != nil {
+			evt.Str("data", base64.StdEncoding.EncodeToString(msg.DecryptedData))
+		}
+		if msg.DecryptedMessage != nil {
+			evt.Str("proto_name", string(msg.DecryptedMessage.ProtoReflect().Descriptor().FullName()))
+		}
+	}
+	evt.Msg("Received response")
+	ch <- msg
+	return true
 }
 
 func (s *SessionHandler) sendMessageWithParams(params SendMessageParams) (*IncomingRPCMessage, error) {
