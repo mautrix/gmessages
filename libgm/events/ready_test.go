fece6ec68113d4f05810a283426b89197d142001
Tulir Asokan <tulir@maunium.net>
2023-09-04 14:25:28 +0300

    Add test for RequestError parsing

---
+++ libgm/events/ready_test.go
@@ -0,0 +1,30 @@
+package events_test
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.mau.fi/mautrix-gmessages/libgm/events"
+	"go.mau.fi/mautrix-gmessages/libgm/gmproto"
+	"go.mau.fi/mautrix-gmessages/libgm/pblite"
+)
+
+func TestRequestError_Is(t *testing.T) {
+	dat, _ := base64.StdEncoding.DecodeString("WzUsIlJlcXVlc3RlZCBlbnRpdHkgd2FzIG5vdCBmb3VuZC4iLFtbInR5cGUuZ29vZ2xlYXBpcy5jb20vZ29vZ2xlLmludGVybmFsLmNvbW11bmljYXRpb25zLmluc3RhbnRtZXNzYWdpbmcudjEuVGFjaHlvbkVycm9yIixbMV1dXV0=")
+	var errResp gmproto.ErrorResponse
+	err := pblite.Unmarshal(dat, &errResp)
+	require.NoError(t, err)
+	assert.ErrorIs(t, events.RequestError{Data: &errResp}, events.ErrRequestedEntityNotFound)
+	assert.NotErrorIs(t, events.RequestError{Data: &errResp}, events.RequestError{
+		Data: &gmproto.ErrorResponse{
+			Type:    5,
+			Message: "meow.",
+			Class: []*gmproto.ErrorResponse_ErrorClass{{
+				Class: "type.googleapis.com/google.internal.communications.instantmessaging.v1.TachyonError",
+			}},
+		},
+	})
+}
--- libgm/go.mod
+++ libgm/go.mod
@@ -5,12 +5,16 @@ go 1.20
 require (
 	github.com/google/uuid v1.3.0
 	github.com/rs/zerolog v1.29.1
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
